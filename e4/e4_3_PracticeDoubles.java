416b266564c37995db1e64e579032ff9bab29e9d
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-19 23:40:29 +0100

    Updated to include equation

--- e4/e4_3_PracticeDoubles.java
+++ e4/e4_3_PracticeDoubles.java
@@ -11,8 +11,8 @@ public class e4_3_PracticeDoubles{
     
     public static void main (String[] args){
         
-        double borrowed;
-        int years, rate;
+        double borrowed, rate;
+        int years;
         
         Scanner kb = new Scanner(System.in);
         
@@ -26,6 +26,16 @@ public class e4_3_PracticeDoubles{
         rate = kb.nextDouble();
         
         //Formulas doesnt appear to make sense, will need to give more thought
+            // its crude and assumes interet rate provided is over full period, but is sufficient for this exercise
+            // I've just rounded, rather than taking the extra penny if required and distributing over the years
+            //  due to that not really being part of the exercise
+        double total = borrowed * ( 1 + rate / 100);
+        double yearlyPayment = total/years;
+        
+        System.out.printf("The total amount to be paid is: £%.2f, \n", total);
+        System.out.printf("at a yearly amount of £%.2f,\n", total / years);
+        System.out.println("and the interest will be paid off after " + Math.round(Math.ceil((total - borrowed ) / yearlyPayment)) + " years"  );
+        //System.out.printf("%.2f", borrowed);
         
         
     }
