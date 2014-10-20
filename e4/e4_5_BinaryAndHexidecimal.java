7f5cd9402da8004c214d77f06dee2c9f0c74277b
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-20 18:47:42 +0100

    Removed superflous code form previous exercise

--- e4/e4_5_BinaryAndHexidecimal.java
+++ e4/e4_5_BinaryAndHexidecimal.java
@@ -70,61 +70,6 @@ public class e4_5_BinaryAndHexidecimal {
         return hex;
     }
     
-        static String Dec2Hex(int decimal)
-        {
-            int highestHexPower = 0, decimalValueRemaining = decimal;
-            String hex = "0x";
-        
-            while (Math.pow(16,highestHexPower) <= decimal)
-            {
-                highestHexPower++;
-            }
-        
-            for (int i = highestHexPower - 1; i >= 0; i--)
-            {
-                int nextHexNumber = (int) Math.floor(decimalValueRemaining / Math.pow(16,i));
-                decimalValueRemaining -= (Math.pow(16,i) * nextHexNumber);
-                hex += DecToHexChar(nextHexNumber);
-            }
-        
-            return hex;
-        }
-    
-    static int binary2decimal(String binary)
-    {
-        int decimal = 0;
-        for (int i = 0; i < binary.length(); i++)
-        {
-            decimal += Math.pow(2,i) * (binary.charAt(binary.length() - 1 - i) == '1' ? 1 : 0);
-        }
-        
-        return decimal;
-    }
-    
-    static String DecToHexChar(int value)
-    {
-        
-        switch (value) {
-            case 0: return "0";
-            case 1: return "1";
-            case 2: return "2";
-            case 3: return "3";
-            case 4: return "4";
-            case 5: return "5";
-            case 6: return "6";
-            case 7: return "7";
-            case 8: return "8";
-            case 9: return "9";
-            case 10: return "a";
-            case 11: return "b";
-            case 12: return "c";
-            case 13: return "d";
-            case 14: return "e";
-            case 15: return "f";
-            default: return "X";
-        }
-    }
-    
 
     static String Hex2Bin(String input)
     {
@@ -143,17 +88,6 @@ public class e4_5_BinaryAndHexidecimal {
         }
         
         return binary;
-    }
-    
-    static int Hex2Decimal(String hex)
-    {
-        int decimal = 0;
-        for (int i = 0; i < hex.length(); i++)
-        {
-            decimal += Math.pow(16,i) * HexCharToDec(hex.charAt(hex.length() - 1 - i));
-        }
-        
-        return decimal;
     }
     
     static String Bin4BitToHex(String binBit)
@@ -200,62 +134,6 @@ public class e4_5_BinaryAndHexidecimal {
             case 'f': return  "1111";
             default: return "X";
         }
-    }
-
-    
-    static int HexCharToDec(char hexChar)
-    {
-    
-        switch (hexChar) {
-            case '0': return 0;
-            case '1': return 1;
-            case '2': return 2;
-            case '3': return 3;
-            case '4': return 4;
-            case '5': return 5;
-            case '6': return 6;
-            case '7': return 7;
-            case '8': return 8;
-            case '9': return 9;
-            case 'a': return 10;
-            case 'b': return 11;
-            case 'c': return 12;
-            case 'd': return 13;
-            case 'e': return 14;
-            case 'f': return 15;
-            default: return -1;
-        }
-        
-    }
-    
-
-    static String Dec2Bin(int number)
-    {
-        int highestPower = 0;
-        String output = "";
-        
-        while (Math.pow(2,highestPower) <= number)
-        {
-            highestPower++;
-        }
-        
-        int remainingNumber = number;
-        
-        for (int i = highestPower - 1; i >=0; i--)
-        {
-            if (Math.pow(2,i) <= remainingNumber)
-            {
-                output += "1";
-                remainingNumber -= Math.pow(2,i);
-            }
-            else
-            {
-                output +="0";
-            }
-            
-        }
-        
-        return output;
     }
 
     
