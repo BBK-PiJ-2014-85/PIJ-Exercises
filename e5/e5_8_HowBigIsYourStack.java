736411dcaabdd5e7543b478cfe4b32bfd509185d
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-21 21:23:11 +0100

    Added ome examples

--- e5/e5_8_HowBigIsYourStack.java
+++ e5/e5_8_HowBigIsYourStack.java
@@ -11,6 +11,8 @@ public class e5_8_HowBigIsYourStack{
 
     static int method1 =0;
     static int method2 =0;
+    static int method3 = 0;
+    static int method4=0;
     
     public static void main(String[] args){
         
@@ -22,8 +24,18 @@ public class e5_8_HowBigIsYourStack{
         catch(StackOverflowError e)
         {System.out.println("Size of stack was " + method2 + " method 2's");}
 
+        method1=0;
         
-        
+        try {method1();}
+        catch(StackOverflowError e)
+        {System.out.println("Size of stack was " + method1 + " method 1's");}
+
+        try {method3();}
+        catch(StackOverflowError e)
+        {System.out.println("Size of stack was " + method3 + " method 3's");}
+        try {method4();}
+        catch(StackOverflowError e)
+        {System.out.println("Size of stack was " + method4 + " method 4's");}
         
     }
 
@@ -39,6 +51,20 @@ public class e5_8_HowBigIsYourStack{
         method2--;
         method2++;
         method2();
+    }
+    
+    static void method3()
+    {
+        String c = null;
+        method3++;
+        method3();
+    }
+    
+    static void method4()
+    {
+        int c = 5;
+        method4++;
+        method4();
     }
 
 
