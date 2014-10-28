c18664eb518139d95cd81f273dc0665d811b4c5b
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-28 22:24:11 +0000

    First commit

---
+++ e6/Matrix2.java
@@ -0,0 +1,35 @@
+
+
+class Matrix2 extends Matrix{
+
+    public void setMatrix(String input)
+    {
+        int rowNum = 0;
+        String rowVals="";
+        //assume input is accurate
+        for (int i=0; i< input.length(); i++)
+        {
+    
+            if (input.charAt(i) == ';')
+            {
+                setRow(rowNum, rowVals);
+                rowNum++;
+                rowVals="";
+            }
+            else
+            {
+                rowVals += input.charAt(i);
+                if (i == input.length() - 1)
+                {
+                    setRow(rowNum, rowVals);
+                }
+            }
+        }
+    
+    }
+
+    public Matrix2(int a, int b)
+    {
+        super(a,b);    }
+
+}
---
+++ e6/e6_4_OneLinerMatrices.java
@@ -0,0 +1,14 @@
+
+class e6_4_OneLinerMatrices{
+
+    public static void main(String[] args)
+    {
+    
+    Matrix2 mat= new Matrix2(4,4);
+    
+    mat.setMatrix("1,2,3,4;5,6,7,8;9,10,11,12;13,14,15,16");
+    mat.prettyPrint();
+    
+    }
+
+}
