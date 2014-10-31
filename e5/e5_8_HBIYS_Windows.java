4d1c62bf08cf0c442793064163a9de8d40e357a9
Paul Day <pday02@mail.bbk.ac.uk>
2014-10-31 13:02:42 +0000

    Added to test Windows command prompt issue not being able to see what is printed

---
+++ e5/e5_8_HBIYS_Windows.java
@@ -0,0 +1,18 @@
+
+class e5_8_HBIYS_Windows{
+
+static int count=0;
+
+public static void main (String[] args)
+{
+    addToStack();
+}
+
+private static void addToStack()
+{
+    count++;
+    System.out.println(count);
+    addToStack();
+}
+
+}
