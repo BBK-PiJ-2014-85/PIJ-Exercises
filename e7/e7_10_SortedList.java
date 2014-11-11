b29d5b1f41746e1ccf0508c0a12f6df095e7c093
Paul Day <pday02@mail.bkk.ac.uk>
2014-11-11 20:30:22 +0000

    First Commit

---
+++ e7/e7_10_SortedList.java
@@ -0,0 +1,21 @@
+package e7;
+
+public class e7_10_SortedList {
+
+	public static void main(String[] args) {
+		
+		ListUtilities list = new ListUtilities(true);
+		
+		list.add(new int[] {1,2,3,4,5,6,7,8,9});
+		list.delete(7);
+		System.out.println("There are " + list.count() + " entries in the list.");
+		list.print();
+		
+		System.out.println("");
+		
+		list.add(new int[] {10, 1});
+		list.print();
+
+	}
+
+}
