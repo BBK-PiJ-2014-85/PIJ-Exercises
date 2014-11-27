fdbd6c7283dfd54bc9ab16e4e66996b2e31eea9b
Paul Day <pday02@mail.bkk.ac.uk>
2014-11-27 09:26:59 +0000

    Added first version of  delete()

--- e9/IntegerTreeNode.java
+++ e9/IntegerTreeNode.java
@@ -4,7 +4,8 @@ public class IntegerTreeNode {
 	int value;
 	IntegerTreeNode right;
 	IntegerTreeNode left;
-	
+
+	boolean isNull = false;
 	
 	public IntegerTreeNode(int num)
 	{
@@ -13,28 +14,92 @@ public class IntegerTreeNode {
 	
 	public void delete(int num)
 	{
-		//unclear what to do with multiple entries (doesn't say is a set) so issue ignored
-		if (num == value) //must be first in tree given logic below
+		
+		if (num == value) // this will always be the first in the tree if met due to logic below
 		{
 			//TODO - check how to delete this object
-			if (right == null && left == null) System.out.println("Error: Last item deleted in tree");
-			// Else what to do if has other value
-		}
-		
-		if (left != null && left.value == num) 
+			if (right == null && left == null) 
+				{
+				isNull= true;
+				System.out.println("Warning: Last item deleted in tree");
+				}
+			else if (left != null)
 			{
-				left = left.left;
+				setLeft();
 			}
-		else if (right !=null && right.value == num) right=right.right;
+			else setRight();
+		}
+		else // check the roots from this point
+		{ // Need to keep looking same side every time
+			
+			while (left != null && left.value == num) //while removes multiple instances of same number
+				// assuming it is not a set
+			{
+				if (left.left == null && left.right == null) left=null;
+				else if (left.left != null) 
+				{
+					left = left.left;
+					left.setLeft();
+				}
+				else left = left.right;
+			}
+		
+			while (right != null && right.value == num)
+			{
+				if (right.left == null && right.right == null) right=null;
+				else if (right.right != null) 
+					{
+					right = right.right;
+					right.setRight();
+					}
+				else right = right.left;
+			}
+			
+			if (num > value && right != null) right.delete(num);
+			else if (left != null) left.delete(num);
+			
+		}
 			
 	}
 	
-	private void setNextLeftValue(boolean Left)
+	
+	private void setLeft()
 	{
-		if (left.value == null))
-		value = left.value;
-		if (left.value != null)
+		if (left != null)
+		{
+			value = left.value;
+			if (left.left == null) left = left.right;
+			else left.setLeft();
+		}
+		else if (right != null)
+		{
+			value = right.value;
+			left = right.left;
+			right = right.right;
+		}
 	}
+	
+	private void setRight ()
+	{
+		if (right != null)
+		{
+			value = right.value;
+			if (right.right == null) right = right.left;
+			else right.setRight();
+		}
+		else if (left != null)
+		{
+			value = left.value;
+			right = left.right;
+			left = left.left;
+		}
+	}
+	
+	public void reBalance()
+	{
+		
+	}
+	
 	
 	public void add(int newNumber) {
 		if (newNumber > this.value) {
@@ -109,3 +174,4 @@ public class IntegerTreeNode {
 	}
 	
 }
+
