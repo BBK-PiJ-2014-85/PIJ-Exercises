f6ab863348fd1c9f8d04f0e1ab4b6780bd432c8b
Paul Day <pday02@mail.bkk.ac.uk>
2014-11-27 07:03:59 +0000

    Changed spacing.

--- e9/IntegerTreeNode.java
+++ e9/IntegerTreeNode.java
@@ -1,14 +1,39 @@
 package e9;
 
 public class IntegerTreeNode {
-
 	int value;
 	IntegerTreeNode right;
 	IntegerTreeNode left;
 	
+	
 	public IntegerTreeNode(int num)
 	{
 		value = num;
+	}
+	
+	public void delete(int num)
+	{
+		//unclear what to do with multiple entries (doesn't say is a set) so issue ignored
+		if (num == value) //must be first in tree given logic below
+		{
+			//TODO - check how to delete this object
+			if (right == null && left == null) System.out.println("Error: Last item deleted in tree");
+			// Else what to do if has other value
+		}
+		
+		if (left != null && left.value == num) 
+			{
+				left = left.left;
+			}
+		else if (right !=null && right.value == num) right=right.right;
+			
+	}
+	
+	private void setNextLeftValue(boolean Left)
+	{
+		if (left.value == null))
+		value = left.value;
+		if (left.value != null)
 	}
 	
 	public void add(int newNumber) {
@@ -55,6 +80,32 @@ public class IntegerTreeNode {
 	{
 		if (left == null) return value;
 		else return left.getMin();
+	}
+	
+	public int depth()
+	{
+		if (left == null && right == null) return 0;
+		else return Math.max( (left != null ? left.depth() + 1: 0),(right != null ? right.depth() + 1: 0)  );
+	}
+	
+	public void delete()
+	{
+		//TODO
+	}
+	
+	public void rebalance()
+	{
+		//TODO
+	}
+	
+	public String toString()
+	{	
+		return "[" + value + " L" + (left==null? "[]" : left.toString()) + " R" + (right==null? "[]" : right.toString()) + "]";
+	}
+	
+	public String toStringSimple()
+	{
+		return "[" + value + (left != null? " " + left.toStringSimple():"") + (right != null? " " + right.toStringSimple():"") + "]";
 	}
 	
 }
