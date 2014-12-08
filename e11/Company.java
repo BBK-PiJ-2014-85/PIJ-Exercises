ef8f02609eb39d997b84460b4a46efc27416c6e5
Paul Day <pday02@mail.bkk.ac.uk>
2014-12-08 19:03:47 +0000

    First commit

---
+++ e11/Company.java
@@ -0,0 +1,9 @@
+package e11;
+
+public class Company {
+	
+	public GenericDoubleLinkedList<String> employeeNames = new GenericDoubleLinkedList<String>();
+	public GenericDoubleLinkedList<Integer> niNumbers = new GenericDoubleLinkedList<Integer>();
+
+	
+}
