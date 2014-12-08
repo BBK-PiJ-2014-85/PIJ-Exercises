f02a2e60f959c863d493a20d77eaef9efe62bbd4
Paul Day <pday02@mail.bkk.ac.uk>
2014-12-08 17:31:16 +0000

    Added exercise 2.1 and 2.2

--- e11/e11.java
+++ e11/e11.java
@@ -1,4 +1,6 @@
 package e11;
+import e10.SmartPhone;
+import e10.MobilePhone;
 
 public class e11 {
 
@@ -9,6 +11,26 @@ public class e11 {
 		System.out.println(comp.getMax("720","360"));
 		System.out.println(comp.getMax(720,360));
 		System.out.println(comp.getMax(720.456,360.234));
+	
+		System.out.println("\nExercise 2 - Upcasting/downcasting\n");
+		
+		System.out.println("Exercise 2.1");
+		SmartPhone myPhone = new SmartPhone("Phone brand"); //constructor requires a string input so added
+		myPhone.browseWeb("www.javaanswers.com");
+		myPhone.findPosition();
+		myPhone.call("012345678");
+
+		System.out.println("Exercise 2.2");
+		System.out.println("Error would be caused by defining SmartPhone methods which would not be available at "
+				+ "MobilePhone level.");
+		System.out.println("A solution is to downcast as below:");
+		MobilePhone myPhone2 = new SmartPhone("Phone brand"); //constructor requires a string input so added
+		((SmartPhone) myPhone2).browseWeb("www.javaanswers.com");
+		((SmartPhone) myPhone2).findPosition();
+		((SmartPhone) myPhone2).call("012345678");
+		
+		
+		
 	}
 	
 }
