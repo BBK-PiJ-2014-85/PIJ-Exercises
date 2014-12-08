fbb6c2a09fe99ad95cf3a7912c825122cb2462c1
Paul Day <pday02@mail.bkk.ac.uk>
2014-12-08 17:48:21 +0000

    Added exercise 2.5

--- e11/e11.java
+++ e11/e11.java
@@ -1,9 +1,12 @@
 package e11;
 import e10.SmartPhone;
 import e10.MobilePhone;
+import e10.Phone;
 
 public class e11 {
 
+	//TODO: Is there a way to check what type has been input?
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Exercise 1 - Don't repeat yourself");
@@ -20,7 +23,7 @@ public class e11 {
 		myPhone.findPosition();
 		myPhone.call("012345678");
 
-		System.out.println("Exercise 2.2");
+		System.out.println("\nExercise 2.2");
 		System.out.println("Error would be caused by defining SmartPhone methods which would not be available at "
 				+ "MobilePhone level.");
 		System.out.println("A solution is to downcast as below:");
@@ -29,9 +32,22 @@ public class e11 {
 		((SmartPhone) myPhone2).findPosition();
 		((SmartPhone) myPhone2).call("012345678");
 		
+		System.out.println("\nExercise 2.3 and 2.4:");
+		testPhone(myPhone2);
+		System.out.println("Only methods defined within the interface, or downcasted can be called with the method.");
 		
-		
-		
+		System.out.println("\nExercise 2.5:");
+		System.out.println("Passing through a MobilePhone and calling a smartPhone would return a runtime error");
+		// The code below would cause this error
+		//MobilePhone mob = new MobilePhone("brand here");
+		//testPhone(mob);
+			
+	}
+	
+	public static void testPhone(Phone inputPhone) 
+	{
+		inputPhone.call("012345");
+		((SmartPhone) inputPhone).browseWeb("www.downcastme.com");
 	}
 	
 }
