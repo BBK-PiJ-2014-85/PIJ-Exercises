4ebf8cfd7ce452d673e8ba9d22f999abc613c17b
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-06 22:14:54 +0000

    First commit.

---
+++ e12/TestSimpleMap.java
@@ -0,0 +1,95 @@
+package e12;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import e7.CircleList;
+
+import e8.SimpleMap;
+import e8.MySimpleMap;
+
+public class TestSimpleMap {
+
+	SimpleMap map;
+
+	@Before
+	public void setUpMap() {
+		map = new MySimpleMap();
+	}
+	
+	
+	@Test
+	public void testIsEmptyAtStart(){
+		
+	}
+	
+	@Test
+	public void testIsEmptyHavingRemoved(){
+		
+	}
+	
+	@Test
+	public void testIsNotEmptyHavingHadOneAdded(){
+		
+	}
+	
+	@Test
+	public void testIsNotEmptyAfterAddRemoveAdd(){
+		
+	}
+	
+	@Test
+	public void testRetrieveOnEmpty()
+	{
+		
+	}
+
+	@Test
+	public void testAddOneAndGet()
+	{
+		
+	}
+	
+	@Test
+	public void testAddSameValueTwice()
+	{
+		
+	}
+	
+	@Test
+	public void testAddRemoveAdd()
+	{
+		
+	}
+	
+	@Test
+	public void testAddRetrieveMatchOnMultiple()
+	{
+		
+	}
+
+	@Test
+	public void testAddRetrieveNoMatchOnMultiple()
+	{
+		
+	}
+	
+	@Test
+	public void testRetrieveSameValueTwice()
+	{
+		
+	}
+	
+	@Test
+	public void testAddingSameValueAfterRemoving()
+	{
+		
+	}
+	
+
+}
