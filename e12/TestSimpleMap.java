e714b122e2f48f5ec6d4f40a0fcc8d0ac7796cb7
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-06 23:01:22 +0000

    $2

--- e12/TestSimpleMap.java
+++ e12/TestSimpleMap.java
@@ -2,13 +2,10 @@ package e12;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+
 
 import org.junit.Before;
 import org.junit.Test;
-
-import e7.CircleList;
 
 import e8.SimpleMap;
 import e8.MySimpleMap;
@@ -25,70 +22,126 @@ public class TestSimpleMap {
 	
 	@Test
 	public void testIsEmptyAtStart(){
-		
+		assertTrue(map.isEmpty());
 	}
 	
 	@Test
 	public void testIsEmptyHavingRemoved(){
-		
+		map.put(5,"five");
+		map.remove(5);
+		assertTrue(map.isEmpty());
 	}
 	
 	@Test
 	public void testIsNotEmptyHavingHadOneAdded(){
-		
+		map.put(5,"five");
+		assertFalse(map.isEmpty());
 	}
 	
 	@Test
 	public void testIsNotEmptyAfterAddRemoveAdd(){
-		
+		map.put(5,"five");
+		map.remove(5);
+		map.put(6, "six");
+		assertFalse(map.isEmpty());
 	}
 	
 	@Test
 	public void testRetrieveOnEmpty()
 	{
-		
+		assertNull(map.get(0));
+	}
+	
+	@Test
+	public void testNullWhenRetrieveNotExist()
+	{
+		map.put(1,"one");
+		assertNull(map.get(2));
 	}
 
 	@Test
 	public void testAddOneAndGet()
 	{
-		
+		map.put(-1, "minus one");
+		assertEquals(map.get(-1),"minus one");
 	}
 	
 	@Test
 	public void testAddSameValueTwice()
 	{
-		
+		map.put(1, "one");
+		map.put(1, "two");
+		assertEquals(map.get(1),"one");
 	}
 	
 	@Test
 	public void testAddRemoveAdd()
 	{
-		
+		map.put(1,"zero");
+		map.remove(1);
+		map.put(1, "one");
+		assertEquals(map.get(1),"one");
 	}
 	
 	@Test
 	public void testAddRetrieveMatchOnMultiple()
 	{
-		
+		map.put(1, "one");
+		map.put(2, "two");
+		map.put(3, "three");
+		assertEquals(map.get(1), "one");
+		assertEquals(map.get(2),"two");
+		assertEquals(map.get(3),"three");
 	}
 
 	@Test
 	public void testAddRetrieveNoMatchOnMultiple()
 	{
-		
+		map.put(1, "one");
+		map.put(2, "two");
+		map.put(3, "three");
+		map.remove(4);
+		assertEquals(map.get(1), "one");
+		assertEquals(map.get(2),"two");
+		assertEquals(map.get(3),"three");
+		assertNull(map.get(4));
+	}
+	
+	@Test
+	public void testRetrieveOnceRemoved()
+	{
+		map.put(1,"one");
+		map.remove(1);
+		assertNull(map.get(1));
+	}
+	
+	@Test
+	public void testRetriveOnceRemovedOnMultiple()
+	{
+		map.put(1, "one");
+		map.put(2, "two");
+		map.put(3, "three");
+		map.remove(2);
+		assertEquals(map.get(1), "one");
+		assertNull(map.get(2));
+		assertEquals(map.get(3),"three");
 	}
 	
 	@Test
 	public void testRetrieveSameValueTwice()
 	{
-		
+		map.put(1, "one");
+		assertEquals(map.get(1), "one");
+		assertEquals(map.get(1), "one");
 	}
 	
 	@Test
 	public void testAddingSameValueAfterRemoving()
 	{
-		
+		map.put(1, "zero");
+		map.remove(1);
+		map.put(1, "one");
+		assertEquals(map.get(1),"one");
 	}
 	
 
