319c5f73af6393adc3ee711d9860fd6752c2bb7b
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-11 19:33:52 +0000

    Changed to be an interface instead, with AnimalImpl implementing this as an Abstract class

--- e10/Animal.java
+++ e10/Animal.java
@@ -1,33 +1,23 @@
 package e10;
 
-public abstract class Animal {
+public interface Animal {
 	
-	private boolean aquatic, flying;
-	private String name;
 		
 	/**
 	* Every animal should have a call which prints on screen
 	*/
-	void call() 
-	{
-		if (flying) System.out.println(name + " now flying. Will come later when tired.");
-		else if (aquatic) System.out.println(name + " will not come.");
-		else System.out.println(name + " coming...");
-	}
+	void call();
 	
 	/**
 	* Every animal should have a method to reproduce after the rain
 	*/	
-	void reproduce() {}
+	void reproduce();
 	
-	public Animal(String name, boolean flys, boolean swims)
-	{
-		this.name = name;
-		this.flying = flys;
-		this.aquatic = swims;
-	}
+	/** 
+	 * Every animal has a means to make sound
+	 */
 	
+	void makeSound();
 	
-	void makeSound() {System.out.println("Make some NOIIIISSSEEEE....(Dont know what I'm meant to do with this)");}
-
 }
+
