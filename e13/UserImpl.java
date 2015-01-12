29b0c2375271490106a5c6b07a975536826769b2
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-12 20:35:53 +0000

    Added methods to add registry

--- e13/UserImpl.java
+++ e13/UserImpl.java
@@ -1,9 +1,11 @@
 package e13;
 
+
 public class UserImpl implements User{
 
 	final private String name;
 	private int libraryID;
+	private Library lib;
 	
 	public UserImpl(String name)
 	{
@@ -21,9 +23,22 @@ public class UserImpl implements User{
 	}
 
 	@Override
-	public void setLibrayID(int id) {
+	public void setLibraryID(int id) {
 		libraryID = id;
 		
+	}
+
+	@Override
+	public void register(Library lib) {
+		this.lib = lib;
+		if (lib != null) setLibraryID(lib.getID());
+		else setLibraryID(0);
+	}
+
+	@Override
+	public Library getLibrary() {
+		//if (lib == null) return null;
+		return lib;
 	}
 
 }
