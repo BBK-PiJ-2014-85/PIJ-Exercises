bd87c85a9cdbe8e8e3016979223d430f0820e737
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-12 18:24:17 +0000

    Methods added to allow compile.

--- e13/BookImpl.java
+++ e13/BookImpl.java
@@ -1,5 +1,28 @@
 package e13;
 
-public class BookImpl {
+public class BookImpl implements Book{
 
+	final private String authorName;
+	final private String title;
+	
+	public BookImpl(String name, String title)
+	{
+		authorName = name;
+		this.title = title;
+	}
+
+	@Override
+	public String getAuthorName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 }
