54ef1708142f7aba8a8804c9aa1622957fab4e7c
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-12 19:34:25 +0000

    First commit.

---
+++ e13/UserImpl.java
@@ -0,0 +1,31 @@
+package e13;
+
+public class UserImpl implements User{
+
+	final private String name;
+	private int libraryID;
+	
+	public UserImpl(String name)
+	{
+		this.name = name;
+	}
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLibraryID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLibrayID(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
