b1e2d065adffd5a7545a4d75b278d8772107c593
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-12 20:34:32 +0000

    Updated to include register

--- e13/User.java
+++ e13/User.java
@@ -1,11 +1,39 @@
 package e13;
 
+/**
+ * Class holds user information on the Library system. 
+ * 
+ * Contains name and library ID
+ * 
+ * @author Paul
+ */
+
 public interface User {
 
+	/**
+	 * Returns the name of the user.
+	 * @return the name
+	 */
 	String getName();
 	
+	/**
+	 * Returns the Library ID of the user. 
+	 * 
+	 * @return the Library ID of the user
+	 */
 	int getLibraryID();
 	
-	void setLibrayID(int id);
+	/**
+	 * Sets the library ID for the user.
+	 * @param id the library ID for the user
+	 */
+	
+	void setLibraryID(int id);
+	
+	void register(Library lib);
+	
+	Library getLibrary();
+	
+	
 	
 }
