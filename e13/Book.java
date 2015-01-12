bea9019d8570dfe87d646aaf1ba19bd63bfe666a
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-12 19:28:32 +0000

    Add documentation comments

--- e13/Book.java
+++ e13/Book.java
@@ -1,8 +1,27 @@
+
 package e13;
+
+/**
+ * A book contains an Author and Title, both of which are final.
+ * 
+ * @author Paul
+ */
 
 public interface Book {
 
+	/**
+	 * Returns the name of the author of the book.
+	 * 
+	 * @return the name of the Author
+	 */
+
 	String getAuthorName();
+
+	/**
+	 * Returns the title of the book.
+	 * 
+	 * @return the title of the book
+	 */
 	
 	String getTitle();
 	
