92d863cf757c35e4d6961e86d30fa56bb47ec15c
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-18 21:33:44 +0000

    Added method to return ID

--- e13/LibraryImpl.java
+++ e13/LibraryImpl.java
@@ -4,6 +4,9 @@ public class LibraryImpl implements Library{
 
 	String name;
 	int maxBooks;
+	String[] names = new String[1];
+	int namesInList;
+	
 	
 	public LibraryImpl(String name)
 	{
@@ -29,7 +32,37 @@ public class LibraryImpl implements Library{
 	@Override 
 	public int getID(String name)
 	{
-		return 0;
+		int loc = getNameLocation(name);
+		
+		if (loc == -1) return addNewID(name);
+		
+		return (loc + 1);
+	}
+	
+	private int getNameLocation(String name)
+	{
+		for (int i=0; i< namesInList; i++)
+		{
+			if (names[i].equals(name)) return i;  
+		}
+		
+		return -1;
+	}
+	
+	private int addNewID(String name)
+	{
+		if (namesInList == names.length) extendArray();
+		
+		names[namesInList] = name;
+		namesInList++;
+		return namesInList;
+	}
+	
+	private void extendArray()
+	{
+		String[] newArray = new String[namesInList * 2];
+		for (int i=0; i<namesInList; i++) newArray[i] = names[i];
+		names = newArray;
 	}
 	
 	
