9230d1456ed7552dfb9a55aca8451b0b82b4fe6a
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-18 20:53:42 +0000

    First commit.

---
+++ e13/TestLibrary.java
@@ -0,0 +1,58 @@
+package e13;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLibrary {
+
+	Library lib;
+	
+	@Before
+	public void createLibrary()
+	{
+		lib = new LibraryImpl("Library Name");
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals("Library Name",lib.getLibrary());
+	}
+
+	@Test
+	public void testMaxBooksZeroAtStart()
+	{
+		assertEquals(0,lib.getMaxBooks());
+	}
+	
+	@Test
+	public void testMaxBooksOnceSet()
+	{
+		lib.setMaxBooks(5);
+		assertEquals(5,lib.getMaxBooks());
+	}
+	
+	@Test
+	public void testMaxBooksChangeCorrectly()
+	{
+		lib.setMaxBooks(5);
+		lib.setMaxBooks(10);
+		assertEquals(10,lib.getMaxBooks());
+	}
+	
+	@Test
+	public void testGetMaxBooksNotAlterValue()
+	{
+		lib.setMaxBooks(5);
+		lib.getMaxBooks();
+		assertEquals(5,lib.getMaxBooks());
+	}
+	
+	
+	
+	
+	
+	
+	
+}
