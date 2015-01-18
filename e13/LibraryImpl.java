1ce9e49e22250d5378c38c9fd642934a55af8c01
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-18 21:04:46 +0000

    Added methods for Library class

--- e13/LibraryImpl.java
+++ e13/LibraryImpl.java
@@ -2,27 +2,28 @@ package e13;
 
 public class LibraryImpl implements Library{
 
+	String name;
+	int maxBooks;
+	
 	public LibraryImpl(String name)
 	{
-		
+		this.name = name;
 	}
 	
 	@Override
 	public String getLibrary()
 	{
-		return "Library name here";
+		return name;
 	}
 
 	@Override
 	public void setMaxBooks(int num) {
-		// TODO Auto-generated method stub
-		
+		maxBooks = num;
 	}
 
 	@Override
 	public int getMaxBooks() {
-		// TODO Auto-generated method stub
-		return 0;
+		return maxBooks;
 	}
 	
 	
