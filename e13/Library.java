e07e05a1b90554b2019d8630e7e7ac283a405798
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-18 20:27:56 +0000

    Replaced with first interface for Library complete with javadoc comments

--- e13/Library.java
+++ e13/Library.java
@@ -1,18 +1,42 @@
 package e13;
 
-public class Library {
+/**
+ * Class represents a library holding all relevant information.
+ * 
+ * Holds the ID and name of the library, as well as the max book limit
+ * per user.
+ * 
+ * @author Paul
+ *
+ */
 
-	//mock object
+public interface Library {
+
+	/**
+	 * Returns the name of the Library.
+	 * @return the name of the library
+	 */
 	
-	public String getLibrary()
-	{
-		return "Library name here";
-	}
+	String getLibrary();
 	
-	public int getID()
-	{
-		return 7;
-	}
+	/**
+	 * Return the ID number of the Library.
+	 * @return the ID of the library
+	 */
+	int getID();
 	
+	/**
+	 * Sets the maximum number of books a user may borrow at any one time.
+	 * @param num the number of books any one user can borrow at one time
+	 */
+	
+	void setMaxBooks(int num);
+	
+	/**
+	 * Returns the maximum amount of books a user can borrow at one one moment
+	 * in time
+	 * @return the maximum number of books a user may borrow at any one point
+	 */
+	int getMaxBooks();
 	
 }
