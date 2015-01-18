3a38ac5e7224bdba625171da11f38d3f906a6b36
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-18 20:29:39 +0000

    First commit.

---
+++ e13/LibraryImpl.java
@@ -0,0 +1,18 @@
+package e13;
+
+public class LibraryImpl {
+
+	//mock object
+	
+	public String getLibrary()
+	{
+		return "Library name here";
+	}
+	
+	public int getID()
+	{
+		return 7;
+	}
+	
+	
+}
