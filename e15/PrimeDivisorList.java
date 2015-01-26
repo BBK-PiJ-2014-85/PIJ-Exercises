1bd7533242d7e3f7721cf5c8e8469c5c18637d75
Paul Day <pday02@mail.bkk.ac.uk>
2015-01-26 20:15:42 +0000

    Added interface methods and javadoc comments

--- e15/PrimeDivisorList.java
+++ e15/PrimeDivisorList.java
@@ -1,5 +1,25 @@
 package e15;
 
+/**
+ * 
+ * Contains a list of prime numbers
+ * 
+ * 
+ * @author Paul Day
+ *
+ */
+
 public interface PrimeDivisorList {
 
+	/**
+	 * Adds a prime number to the list
+	 * 
+	 * @throws NullPointerException if the user passes a null reference
+	 * @throws IllegalArgumentException if the user passes a non-prime number
+	 * 
+	 * @param i the integer to be added
+	 */
+	
+	void add(Integer i);
+	
 }
